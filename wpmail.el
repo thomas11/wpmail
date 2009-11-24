7831bb136b86b9434ae5b021124d7bb97e25533f
Thomas Kappler <thomas@linux-dmcx.site>
2009-11-24 21:20:40 +0100

    Wording of docs.

--- wpmail.el
+++ wpmail.el
@@ -24,19 +24,19 @@
 
 ;;; Commentary:
 
-;; A number of functions to make posting by e-mail to the
-;; wordpress.com blog hosting service <http://www.wordpress.com>
-;; easier.  It might work with other wordpress installations, which I
-;; have not tried.  For more information about posting to wordpress by
-;; e-mail see the support page
-;; <http://support.wordpress.com/post-by-email/>.
+;; An Emacs extension to make posting by e-mail to the wordpress.com
+;; blog hosting service <http://www.wordpress.com> easier.  It might
+;; work with other wordpress installations, which I have not tried.
+;; For more information about posting to wordpress by e-mail see the
+;; support page <http://support.wordpress.com/post-by-email/>.
 
-;; Start a new post, possibly from the region or the buffer, with
-;; wpmail-new-post or wpmail-new-post-here. Send it with
-;; wpmail-send-post when you are done.  wpmail will prompt for title
-;; and category; it will propose some titles that you can see via M-n,
-;; and it auto-completes the categories in wpmail-categories.  See the
-;; documentation of these functions for details.
+;; Documentation is a bit lacking, but here's the gist: start a new
+;; post, possibly from the region or the buffer, with wpmail-new-post
+;; or wpmail-new-post-here. Send it with wpmail-send-post when you are
+;; done.  wpmail will prompt for title and category; it will propose
+;; some titles that you can see via M-n, and it auto-completes the
+;; categories in wpmail-categories.  See the documentation of these
+;; functions for details.
 
 ;; You can write your posts in Markdown format
 ;; <http://daringfireball.net/projects/markdown/> if you have
